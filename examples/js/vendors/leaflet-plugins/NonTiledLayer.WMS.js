79d9df367677c19e35a01c5a01ad50fc1f90dc4f
Biel Frontera <bfrontera@socib.es>
2016-07-26 13:51:15 +0200

    Update Leaflet.NonTiledLayer in examples

--- examples/js/vendors/leaflet-plugins/NonTiledLayer.WMS.js
+++ examples/js/vendors/leaflet-plugins/NonTiledLayer.WMS.js
@@ -1,5 +1,5 @@
 /*
- * L.NonTiledLayer.WMS is used for putting WMS tile layers on the map.
+ * L.NonTiledLayer.WMS is used for putting WMS non tiled layers on the map.
  */
 L.NonTiledLayer.WMS = L.NonTiledLayer.extend({
 
@@ -13,17 +13,23 @@ L.NonTiledLayer.WMS = L.NonTiledLayer.extend({
         transparent: false
     },
 
+    options: {
+        crs: null,
+        uppercase: false
+    },
+
     initialize: function (url, options) { // (String, Object)
         this._wmsUrl = url;
 
         var wmsParams = L.extend({}, this.defaultWmsParams);
-
-        for (var i in options) {
-            // all keys that are not TileLayer options go to WMS params
-            if (!this.options.hasOwnProperty(i)) {
-                wmsParams[i] = options[i];
-            }
-        }
+		
+		// all keys that are not NonTiledLayer options go to WMS params
+		for (var i in options) {
+			if (!L.NonTiledLayer.prototype.options.hasOwnProperty(i) && 
+                !L.Layer.prototype.options.hasOwnProperty(i)) {
+				wmsParams[i] = options[i];
+			}
+		}
 
         this.wmsParams = wmsParams;
 
@@ -31,8 +37,12 @@ L.NonTiledLayer.WMS = L.NonTiledLayer.extend({
     },
 
     onAdd: function (map) {
-        var projectionKey = parseFloat(this.wmsParams.version) >= 1.3 ? 'crs' : 'srs';
-        this.wmsParams[projectionKey] = map.options.crs.code;
+
+        this._crs = this.options.crs || map.options.crs;
+        this._wmsVersion = parseFloat(this.wmsParams.version);
+
+        var projectionKey = this._wmsVersion >= 1.3 ? 'crs' : 'srs';
+        this.wmsParams[projectionKey] = this._crs.code;
 
         L.NonTiledLayer.prototype.onAdd.call(this, map);
     },
@@ -42,13 +52,18 @@ L.NonTiledLayer.WMS = L.NonTiledLayer.extend({
         wmsParams.width = width;
         wmsParams.height = height;
 
-        var crs = this._map.options.crs;
-			
-        var p1 = crs.project(world1);
-        var p2 = crs.project(world2);
+        var nw = this._crs.project(world1);
+        var se = this._crs.project(world2);
 
-        var url = this._wmsUrl + L.Util.getParamString(wmsParams, this._wmsUrl) + '&bbox=' + p1.x + ',' + p2.y + ',' + p2.x + ',' + p1.y;
-        return url;
+        var url = this._wmsUrl;
+
+        var bbox = bbox = (this._wmsVersion >= 1.3 && this._crs === L.CRS.EPSG4326 ?
+        [se.y, nw.x, nw.y, se.x] :
+        [nw.x, se.y, se.x, nw.y]).join(',');
+
+        return url +
+			L.Util.getParamString(this.wmsParams, url, this.options.uppercase) +
+			(this.options.uppercase ? '&BBOX=' : '&bbox=') + bbox;
     },
 
     setParams: function (params, noRedraw) {
--- examples/js/vendors/leaflet-plugins/NonTiledLayer.js
+++ examples/js/vendors/leaflet-plugins/NonTiledLayer.js
@@ -1,53 +1,57 @@
-﻿/*
+/*
  * L.NonTiledLayer is an addon for leaflet which renders dynamic image overlays
  */
-L.NonTiledLayer = L.Class.extend({
- 	includes: L.Mixin.Events,
-	options: {
+L.NonTiledLayer = L.Layer.extend({
+    includes: L.Mixin.Events,
+    options: {
         attribution: '',
-		opacity: 1,
-		pane: null
-	},
+        opacity: 1.0,
+        zIndex: undefined,
+        minZoom: 0,
+        maxZoom: 18,
+        pointerEvents: null,
+        errorImageUrl: 'data:image/gif;base64,R0lGODlhAQABAHAAACH5BAUAAAAALAAAAAABAAEAAAICRAEAOw==', //1px transparent GIF
+        bounds: L.latLngBounds([-85.05, -180], [85.05, 180])
+    },
+    url: '',
 
     // override this method in the inherited class
-    getImageUrl: function (world1, world2, width, height) {},
+    //getImageUrl: function (world1, world2, width, height) {},
+    //getImageUrlAsync: function (world1, world2, width, height, f) {},
 
     initialize: function (options) {
-        this._currentImage = null;
-		this._bufferImage = null;
         L.setOptions(this, options);
     },
 
     onAdd: function (map) {
         this._map = map;
 
-        if(!this._div)
+        if (!this._div) {
             this._div = L.DomUtil.create('div', 'leaflet-image-layer');
+            if (this.options.pointerEvents) {
+                this._div.style['pointer-events'] = this.options.pointerEvents;
+            }
+            if (this.options.zIndex !== undefined) {
+                this._div.style.zIndex = this.options.zIndex;
+            }
+            if (this.options.opacity !== undefined){
+                this._div.style.opacity = this.options.opacity;
+            }
+        }
 
-		if(this.options.pane)
-			this._pane = this.options.pane;
-		else
-			this._pane = this._map.getPanes().overlayPane;
+        this.getPane().appendChild(this._div);
+
+        this._bufferImage = this._initImage();
+        this._currentImage = this._initImage();
 
         this._update();
-
-		if (map.options.zoomAnimation && L.Browser.any3d) {
-			map.on('zoomanim', this._animateZoom, this);
-		}
-
-		this._map.on('moveend', this._update, this);
-
-        this._pane.appendChild(this._div);
-	},
+    },
 
     onRemove: function (map) {
- 		this._pane.removeChild(this._div);
+        this.getPane().removeChild(this._div);
 
- 		this._map.off('moveend', this._update, this);
-
-		if (map.options.zoomAnimation) {
-			map.off('zoomanim', this._animateZoom, this);
-		}
+        this._div.removeChild(this._bufferImage);
+        this._div.removeChild(this._currentImage);
     },
 
     addTo: function (map) {
@@ -55,82 +59,112 @@ L.NonTiledLayer = L.Class.extend({
         return this;
     },
 
-	setOpacity: function (opacity) {
-		this.options.opacity = opacity;
-		this._updateOpacity();
-		return this;
-	},
+    getEvents: function () {
+        var events = {
+            moveend: this._update,
+            zoom: this._viewreset
+        };
 
-	// TODO remove bringToFront/bringToBack duplication from TileLayer/Path
-	bringToFront: function () {
-	    if (this._div) {
-			this._pane.appendChild(this._div);
-		}
-		return this;
-	},
+        if (this._zoomAnimated) {
+            events.zoomanim = this._animateZoom;
+        }
 
-	bringToBack: function () {
-		if (this._div) {
-		    this._pane.insertBefore(this._div, this._pane.firstChild);
-		}
-		return this;
-	},
+        return events;
+    },
 
+    getElement: function () {
+        return this._div;
+    },
+
+    setOpacity: function (opacity) {
+        this.options.opacity = opacity;
+        if (this._div) {
+            L.DomUtil.setOpacity(this._div, this.options.opacity);
+        }
+        return this;
+    },
+    
+    setZIndex: function (zIndex) {
+        if(zIndex){
+            this.options.zIndex = zIndex;
+            if (this._div) {
+                this._div.style.zIndex = zIndex;
+            }
+        }
+        return this;
+    },
+
+    // TODO remove bringToFront/bringToBack duplication from TileLayer/Path
+    bringToFront: function () {
+        if (this._div) {
+            this._pane.appendChild(this._div);
+        }
+        return this;
+    },
+
+    bringToBack: function () {
+        if (this._div) {
+            this._pane.insertBefore(this._div, this._pane.firstChild);
+        }
+        return this;
+    },
 
     getAttribution: function () {
         return this.options.attribution;
     },
 
+    _initImage: function (_image) {
+        var _image = L.DomUtil.create('img', 'leaflet-image-layer');
 
-	_initImage: function () {
-		var _image = L.DomUtil.create('img', 'leaflet-image-layer');
-		this._div.appendChild(_image);
+        
+        this._div.appendChild(_image);
 
-		if (this._map.options.zoomAnimation && L.Browser.any3d) {
-			L.DomUtil.addClass(_image, 'leaflet-zoom-animated');
-		} else {
-			L.DomUtil.addClass(_image, 'leaflet-zoom-hide');
-		}
+        if (this._map.options.zoomAnimation && L.Browser.any3d) {
+            L.DomUtil.addClass(_image, 'leaflet-zoom-animated');
+        } else {
+            L.DomUtil.addClass(_image, 'leaflet-zoom-hide');
+        }
 
-		this._updateOpacity();
 
-		//TODO createImage util method to remove duplication
-		L.extend(_image, {
-			galleryimg: 'no',
-			onselectstart: L.Util.falseFn,
-			onmousemove: L.Util.falseFn,
-			onload: L.bind(this._onImageLoad, this)
-		});
 
-		return _image;
+        //TODO createImage util method to remove duplication
+        L.extend(_image, {
+            galleryimg: 'no',
+            onselectstart: L.Util.falseFn,
+            onmousemove: L.Util.falseFn,
+            onload: L.bind(this._onImageLoad, this),
+            onerror: L.bind(this._onImageError, this)
+        });
+
+        return _image;
+    },
+
+    redraw: function () {
+        if (this._map) {
+            this._update();
+        }
+        return this;
     },
 
     _animateZoom: function (e) {
-		if(this._currentImage)
-			this._animateImage(this._currentImage, e);
-		if(this._bufferImage)
-			this._animateImage(this._bufferImage, e);
-	},
+        if (this._currentImage._bounds)
+            this._animateImage(this._currentImage, e);
+        if (this._bufferImage._bounds)
+            this._animateImage(this._bufferImage, e);
+    },
 
     _animateImage: function (image, e) {
-        var map = this._map,
-		    scale = map.getZoomScale(e.zoom),
-		    nw = image._bounds.getNorthWest(),
-		    se = image._bounds.getSouthEast(),
+        var scale = this._map.getZoomScale(e.zoom),
+            offset = this._map._latLngToNewLayerPoint(image._bounds.getNorthWest(), e.zoom, e.center);
 
-		    topLeft = map._latLngToNewLayerPoint(nw, e.zoom, e.center),
-		    size = map._latLngToNewLayerPoint(se, e.zoom, e.center)._subtract(topLeft),
-		    origin = topLeft._add(size._multiplyBy((1 / 2) * (1 - 1 / scale)));
-
-        image.style[L.DomUtil.TRANSFORM] =
-		        L.DomUtil.getTranslateString(origin) + ' scale(' + scale + ') ';
+        L.DomUtil.setTransform(image, offset, scale);
     },
 
     _resetImage: function (image) {
         var bounds = new L.Bounds(
-		        this._map.latLngToLayerPoint(image._bounds.getNorthWest()),
-		        this._map.latLngToLayerPoint(image._bounds.getSouthEast())),
-		    size = bounds.getSize();
+                this._map.latLngToLayerPoint(image._bounds.getNorthWest()),
+                this._map.latLngToLayerPoint(image._bounds.getSouthEast())),
+            size = bounds.getSize();
 
         L.DomUtil.setPosition(image, bounds.min);
 
@@ -138,28 +172,51 @@ L.NonTiledLayer = L.Class.extend({
         image.style.height = size.y + 'px';
     },
 
-    _getClippedBounds: function() {
+    _getClippedBounds: function () {
         var wgsBounds = this._map.getBounds();
 
         // truncate bounds to valid wgs bounds
-        var lon1 = wgsBounds.getNorthWest().lng;
-        var lat1 = wgsBounds.getNorthWest().lat;
-        var lon2 = wgsBounds.getSouthEast().lng;
-        var lat2 = wgsBounds.getSouthEast().lat;
-        lon1 = (lon1 + 180) % 360 - 180;
-        if (lat1 > 85.05) lat1 = 85.05;
-        if (lat2 < -85.05) lat2 = -85.05;
-        if (lon1 < -180) lon1 = -180;
-        if (lon2 > 180) lon2 = 180;
-        var world1 = new L.LatLng(lat1, lon1);
-        var world2 = new L.LatLng(lat2, lon2);
+        var mSouth = wgsBounds.getSouth();
+        var mNorth = wgsBounds.getNorth();
+        var mWest = wgsBounds.getWest();
+        var mEast = wgsBounds.getEast();
+
+        var lSouth = this.options.bounds.getSouth();
+        var lNorth = this.options.bounds.getNorth();
+        var lWest = this.options.bounds.getWest();
+        var lEast = this.options.bounds.getEast();
+
+        //mWest = (mWest + 180) % 360 - 180;
+        if (mSouth < lSouth) mSouth = lSouth;
+        if (mNorth > lNorth) mNorth = lNorth;
+        if (mWest < lWest) mWest = lWest;
+        if (mEast > lEast) mEast = lEast;
+
+        var world1 = new L.LatLng(mNorth, mWest);
+        var world2 = new L.LatLng(mSouth, mEast);
 
         return new L.LatLngBounds(world1, world2);
     },
 
+    _viewreset: function () {
+        if (this._bufferImage._bounds)
+            this._resetImage(this._bufferImage);
+        if (this._currentImage._bounds)
+            this._resetImage(this._currentImage);
+    },
+
     _update: function () {
-		if(this._bufferImage)
-			this._resetImage(this._bufferImage);
+        if (this._map.getZoom() < this.options.minZoom ||
+            this._map.getZoom() > this.options.maxZoom) {
+            this._div.style.visibility = 'hidden';
+            return;
+        }
+        else {
+            this._div.style.visibility = 'visible';
+        }
+
+        if (this._bufferImage._bounds)
+            this._resetImage(this._bufferImage);
 
         var bounds = this._getClippedBounds();
 
@@ -175,44 +232,52 @@ L.NonTiledLayer = L.Class.extend({
         if (width < 32 || height < 32)
             return;
 
-        var url = this.getImageUrl(bounds.getNorthWest(), bounds.getSouthEast(), width, height);
-
-        this._currentImage = this._initImage();
-
         this._currentImage._bounds = bounds;
 
         this._resetImage(this._currentImage);
 
-        this._currentImage.src = url;
+        var i = this._currentImage;
+        if (this.getImageUrl)
+            i.src = this.getImageUrl(bounds.getNorthWest(), bounds.getSouthEast(), width, height);
+        else
+            this.getImageUrlAsync(bounds.getNorthWest(), bounds.getSouthEast(), width, height, function (url, tag) {
+                i.src = url;
+                i.tag = tag;
+            });
 
-		L.DomUtil.setOpacity(this._currentImage, 0);
+        this.url = i.src;
+
+        L.DomUtil.setOpacity(this._currentImage, 0);
     },
-
-    _onImageLoad: function (e) {
-		if(e.target != this._currentImage) { // obsolete image
-            this._div.removeChild(e.target);
-			return;
-		}
-
-        if(this._bufferImage)
-            this._div.removeChild(this._bufferImage);
-
-		this._bufferImage = this._currentImage;
-
-		L.DomUtil.setOpacity(this._currentImage, 1);
-
-	    this.fire('load');
+	_onImageError:function(e){
+		this.fire('error', e);
+		L.DomUtil.addClass(e.target, 'invalid');
+        if(e.target.src !== this.options.errorImageUrl){ // prevent error loop if error image is not valid
+            e.target.src = this.options.errorImageUrl;
+            this._onImageDone(false, e);
+        }
+	},
+    _onImageLoad:function(e){
+        if(e.target.src !== this.options.errorImageUrl){
+            L.DomUtil.removeClass(e.target, 'invalid');
+            if (e.target.src !== this.url) { // obsolete image
+                return;
+            }
+            this._onImageDone(true, e);
+        }
     },
+	_onImageDone: function (success, e) {
+        L.DomUtil.setOpacity(this._currentImage, 1);
+        L.DomUtil.setOpacity(this._bufferImage, 0);
 
-    setOpacity: function (opacity) {
-        this.options.opacity = opacity;
-        this._updateOpacity();
-        return this;
-    },
-
-	_updateOpacity: function () {
-		L.DomUtil.setOpacity(this._div, this.options.opacity);
-	}
+        if (this._addInteraction)
+            this._addInteraction(this._currentImage.tag);
+        
+        var tmp = this._bufferImage;
+        this._bufferImage = this._currentImage;
+        this._currentImage = tmp;
+        this.fire('load', e);
+    }
 });
 
 L.nonTiledLayer = function () {
