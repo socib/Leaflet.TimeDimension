9ef51eb8d6d2c7ad1bddf45b1110f983ea978d00
Biel Frontera <biel.fb@gmail.com>
2019-11-26 00:08:34 +0100

    New example: 17, basic GeoJSON usage

---
+++ examples/js/example17.js
@@ -0,0 +1,61 @@
+function addGeoJSONLayer(map, data) {
+    var icon = L.icon({
+        iconUrl: 'img/bus.png',
+        iconSize: [22, 22],
+        iconAnchor: [11, 11]
+    });
+
+    var geoJSONLayer = L.geoJSON(data, {
+        pointToLayer: function (feature, latLng) {
+            if (feature.properties.hasOwnProperty('last')) {
+                return new L.Marker(latLng, {
+                    icon: icon
+                });
+            }
+            return L.circleMarker(latLng);
+        }
+    });
+
+    var geoJSONTDLayer = L.timeDimension.layer.geoJson(geoJSONLayer, {
+        updateTimeDimension: true,
+        duration: 'PT2M',
+        updateTimeDimensionMode: 'replace',
+        addlastPoint: true
+    });
+
+    // Show both layers: the geoJSON layer to show the whole track
+    // and the timedimension layer to show the movement of the bus 
+    geoJSONLayer.addTo(map);
+    geoJSONTDLayer.addTo(map);
+}
+
+var map = L.map('map', {
+    zoom: 14,
+    fullscreenControl: true,
+    timeDimensionControl: true,
+    timeDimensionControlOptions: {
+        timeSliderDragUpdate: true,
+        loopButton: true,
+        autoPlay: true,
+        playerOptions: {
+            transitionTime: 1000,
+            loop: true
+        }
+    },
+    timeDimension: true,
+    center: [36.72, -4.43]
+}); 
+
+var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+});
+osmLayer.addTo(map);
+
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", (function (xhr) {
+    var response = xhr.currentTarget.response;
+    var data = JSON.parse(response);
+    addGeoJSONLayer(map, data);
+}));
+oReq.open('GET', 'data/track_bus699.geojson');
+oReq.send();
